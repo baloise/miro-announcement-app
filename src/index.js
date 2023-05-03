06580b6ed918091b4b5692180dc241959a2c5f01
Matthias Cullmann <matthias.cullmann@baloise.com>
2023-05-03 15:49:01 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-async function init() {
-  miro.board.ui.on('icon:click', async () => {
-    await miro.board.ui.openPanel({url: 'app.html'});
-  });
+async function announce() {
+	console.log("open announcement")
+	//await miro.board.ui.openPanel({url: 'app.html'})
 }
 
-init();
+async function init() {
+	await announce()
+	miro.board.ui.on('icon:click', async () => {
+		await announce()
+  	})
+}
+
+init()
