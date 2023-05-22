5ef0d7261c4f9e605c38b02a727b14da2067330d
Matthias Cullmann <matthias.cullmann@baloise.com>
2023-05-22 16:47:40 +0200

    added pilot users

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ var announcements = [
 	'2023-12.html',
 ]
 
+
 async function announce() {
+	var user = await miro.board.getUserInfo()
+	if(![
+  "3074457347386012337", // Matthias
+  "3074457347728925407",  // Laurent 
+  "3074457361962338134",  // David
+].includes(user.id)
+	){ return;}
 	console.log("announcement")
 	announcements.forEach(a => {
 		if(localStorage.getItem("dontShow/"+a)!="true"){
